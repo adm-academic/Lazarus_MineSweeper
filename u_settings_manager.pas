88c9ee85008d18064598bb81ef04df2ecb6db61d
dmitryalexeev <dmitryalexeev@localhost>
2024-06-23 11:24:30 +0700

    Добавил "Менеджер Настроек" и закодил форму с настройками на его основе. Настройки автоматически сохраняются и читаются из INI-файл.

--- u_settings_manager.pas
+++ u_settings_manager.pas
@@ -10,8 +10,11 @@ unit u_settings_manager;
 interface
 
 uses
-  Classes, SysUtils,
+  Classes, SysUtils, IniFiles,
   u_minesweeper_types;
+
+const
+   filename = 'settings.ini';
 
 type
 
@@ -23,26 +26,249 @@ type
 
   T_Settings_Manager = class
   private
+    asset_packs_list : TStringList;
+    function get_config_filename : string;
   protected
   public
-    constructor Create;
-    destructor Destroy; override;
+    constructor Create; // конструктор
+    destructor Destroy; override; // деструктор
+
+    function  get_asset_pack_name:String; // геттер для текущего имени ассет пака
+    procedure set_asset_pack_name(asset_pack_name:String);// сеттер для текущего имени ассет пака
+
+    function  get_asset_packs_list : TStringList; // список всех доступных ассет паков
+
+    function  get_music_state_on : boolean;    // геттер для вкл. музыки
+    procedure set_music_state_on( music_on : boolean ); // сеттер для вко. музыки
+
+    function  get_sounds_state_on : boolean; // геттер для вкл. звуков
+    procedure set_sounds_state_on( sounds_on : boolean ); // сеттер для вкл. звуков
+
+
+    function  get_sounds_volume : integer;  // геттер для громкости звуков
+    procedure set_sounds_volume( volume: integer ); // сеттер для громкости звуков
+
+
+    function  get_music_volume : integer; // геттер для громкоси музыки
+    procedure set_music_volume( volume: integer ); // сеттер для громкости музыки
+
   end;
 
 implementation
+uses
+  Dialogs,Forms;
 
 { T_Settings_Manager }
+
+function T_Settings_Manager.get_config_filename: string;
+begin
+  Result := ExtractFilePath(Application.ExeName) + PathDelim + filename;
+end;
 
 constructor T_Settings_Manager.Create;
 begin
   inherited Create;
-  //...
+  self.asset_packs_list:=nil;
+
 end;
 
 destructor T_Settings_Manager.Destroy;
 begin
-  //...
+
   inherited Destroy;
+end;
+
+function T_Settings_Manager.get_asset_pack_name: String;
+var
+  IniF:TINIFile;
+  ff: string;
+begin
+  ff:=self.get_config_filename;
+  if ( not FileExists(ff) ) then exit;
+  IniF:=TIniFile.Create(ff);
+
+  Result:= INiF.ReadString('general','asset_pack_name','asset_pack_blue');
+
+  FreeAndNil(IniF);
+end;
+
+procedure T_Settings_Manager.set_asset_pack_name(asset_pack_name: String);
+var
+  IniF:TINIFile;
+  ff: string;
+begin
+  ff:=self.get_config_filename;
+  IniF:=TIniFile.Create(ff);
+
+  IniF.WriteString('general','asset_pack_name',asset_pack_name);
+
+  FreeAndNil(IniF);
+end;
+
+function T_Settings_Manager.get_asset_packs_list: TStringList;
+var
+  sr: TSearchRec;
+  path: string;
+begin
+  if ( Assigned(self.asset_packs_list) ) then // если уже есть список строк для ассет-паков, то удалим его
+    FreeAndNil(self.asset_packs_list);
+
+  self.asset_packs_list:=TStringList.Create; // создадим список строк для ассет-паков
+
+  path:=ExtractFilePath(Application.ExeName) + 'asset_packs' + PathDelim;
+
+  if FindFirst(path + '*', faAnyFile, sr) = 0 then
+  begin
+    repeat
+      if (sr.Attr and faDirectory) <> 0 then  // если найденный файл - папка
+        begin
+        if (sr.Name <> '.') and (sr.Name <> '..') then  // игнорировать служебные папки
+          begin
+            self.asset_packs_list.Append(sr.Name);
+          end;
+        end;
+    until FindNext(sr) <> 0;
+  end;
+  FindClose(sr);
+
+  Result:=self.asset_packs_list;
+
+end;
+
+function T_Settings_Manager.get_music_state_on: boolean;
+var
+  IniF:TINIFile;
+  ff: string;
+begin
+  ff:=self.get_config_filename;
+  if ( not FileExists(ff) ) then exit;
+  IniF:=TIniFile.Create(ff);
+
+  Result:= LowerCase( INiF.ReadString('sound','music_state','off') ) = 'on';
+
+  FreeAndNil(IniF);
+end;
+
+procedure T_Settings_Manager.set_music_state_on(music_on: boolean);
+var
+  IniF:TINIFile;
+  ff: string;
+  state : string;
+begin
+  ff:=self.get_config_filename;
+  IniF:=TIniFile.Create(ff);
+
+  if music_on then
+    state:='on'
+  else
+    state:='off';
+
+  IniF.WriteString('sound','music_state',state);
+
+  FreeAndNil(IniF);
+end;
+
+function T_Settings_Manager.get_sounds_state_on: boolean;
+var
+  IniF:TINIFile;
+  ff: string;
+begin
+  ff:=self.get_config_filename;
+  if ( not FileExists(ff) ) then exit;
+  IniF:=TIniFile.Create(ff);
+
+  Result:= LowerCase( INiF.ReadString('sound','sounds_state','off') ) = 'on';
+
+  FreeAndNil(IniF);
+end;
+
+procedure T_Settings_Manager.set_sounds_state_on(sounds_on: boolean);
+var
+  IniF:TINIFile;
+  ff: string;
+  state : string;
+begin
+  ff:=self.get_config_filename;
+  IniF:=TIniFile.Create(ff);
+
+  if sounds_on then
+    state:='on'
+  else
+    state:='off';
+
+  IniF.WriteString('sound','sounds_state',state);
+
+  FreeAndNil(IniF);
+end;
+
+function T_Settings_Manager.get_sounds_volume: integer;
+var
+  IniF:TINIFile;
+  ff: string;
+begin
+  ff:=self.get_config_filename;
+  if ( not FileExists(ff) ) then exit;
+  IniF:=TIniFile.Create(ff);
+
+  Result:= IniF.ReadInteger('sound','sounds_volume',50);
+  if (Result<0)or (Result>100) then Result:=50;
+
+  FreeAndNil(IniF);
+end;
+
+procedure T_Settings_Manager.set_sounds_volume(volume: integer);
+var
+  IniF:TINIFile;
+  ff: string;
+  tmp_vol: integer;
+begin
+  ff:=self.get_config_filename;
+  IniF:=TIniFile.Create(ff);
+
+  if (volume<0) or (volume>100) then
+    tmp_vol:=50
+  else
+    tmp_vol:=volume;
+
+
+  IniF.WriteInteger('sound','sounds_volume',tmp_vol);
+
+  FreeAndNil(IniF);
+end;
+
+function T_Settings_Manager.get_music_volume: integer;
+var
+  IniF:TINIFile;
+  ff: string;
+begin
+  ff:=self.get_config_filename;
+  if ( not FileExists(ff) ) then exit;
+  IniF:=TIniFile.Create(ff);
+
+  Result:= IniF.ReadInteger('sound','music_volume',50);
+  if (Result<0)or (Result>100) then Result:=50;
+
+  FreeAndNil(IniF);
+end;
+
+procedure T_Settings_Manager.set_music_volume(volume: integer);
+var
+  IniF:TINIFile;
+  ff: string;
+  tmp_vol: integer;
+begin
+  ff:=self.get_config_filename;
+  IniF:=TIniFile.Create(ff);
+
+  if (volume<0) or (volume>100) then
+    tmp_vol:=50
+  else
+    tmp_vol:=volume;
+
+
+  IniF.WriteInteger('sound','music_volume',tmp_vol);
+
+  FreeAndNil(IniF);
 end;
 
 end.
--- uf_settings.pas
+++ uf_settings.pas
@@ -5,14 +5,36 @@ unit uf_settings;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs;
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, ComCtrls,
+  u_settings_manager;
 
 type
+
+  { Tf_settings }
+
   Tf_settings = class(TForm)
+    bt_save_all: TButton;
+    cb_play_music: TCheckBox;
+    cb_select_asset_pack: TComboBox;
+    cb_play_sounds: TCheckBox;
+    lb_music_volume: TLabel;
+    lb_sounds_volume: TLabel;
+    lb_select_asset_pack: TLabel;
+    tb_music_volume: TTrackBar;
+    tb_sounds_volume: TTrackBar;
+    procedure bt_save_allClick(Sender: TObject);
+    procedure cb_play_musicChange(Sender: TObject);
+    procedure cb_play_soundsChange(Sender: TObject);
+    procedure cb_select_asset_packChange(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+    procedure FormDestroy(Sender: TObject);
+    procedure FormShow(Sender: TObject);
+    procedure tb_music_volumeChange(Sender: TObject);
+    procedure tb_sounds_volumeChange(Sender: TObject);
   private
 
   public
-
+    settings_manager : T_Settings_Manager;
   end;
 
 var
@@ -21,6 +43,71 @@ var
 implementation
 
 {$R *.lfm}
+
+{ Tf_settings }
+
+procedure Tf_settings.FormCreate(Sender: TObject);// при создании формы
+begin
+  self.settings_manager:=T_Settings_Manager.Create;
+end;
+
+procedure Tf_settings.cb_select_asset_packChange(Sender: TObject); // при изменении значения в комбо-боксе
+begin
+  self.settings_manager.set_asset_pack_name(self.cb_select_asset_pack.Text);
+end;
+
+procedure Tf_settings.bt_save_allClick(Sender: TObject);  // при нажатии на кнопку "Сохранить Всё"
+begin
+  { сохраняем по-порядочку из контролов на форме }
+  self.settings_manager.set_asset_pack_name(self.cb_select_asset_pack.Text);
+  self.settings_manager.set_music_state_on(self.cb_play_music.Checked);
+  self.settings_manager.set_sounds_state_on(self.cb_play_sounds.Checked);
+  self.settings_manager.set_sounds_volume(self.tb_sounds_volume.Position);
+  self.settings_manager.set_music_volume(self.tb_music_volume.Position);
+end;
+
+procedure Tf_settings.cb_play_musicChange(Sender: TObject);
+begin
+  self.settings_manager.set_music_state_on(self.cb_play_music.Checked);
+end;
+
+procedure Tf_settings.cb_play_soundsChange(Sender: TObject);
+begin
+ self.settings_manager.set_sounds_state_on(self.cb_play_sounds.Checked);
+end;
+
+procedure Tf_settings.FormDestroy(Sender: TObject); // при уничтожении формы
+begin
+ FreeAndNil(self.settings_manager);
+end;
+
+procedure Tf_settings.FormShow(Sender: TObject); // при показе формы
+var
+  i: integer;
+  apn : string;
+begin
+  { заполняем комбо-бокс с ассет паками }
+  self.cb_select_asset_pack.Items.SetStrings(self.settings_manager.get_asset_packs_list);
+  apn :=  self.settings_manager.get_asset_pack_name;
+  i:= self.cb_select_asset_pack.Items.IndexOf( apn );
+  self.cb_select_asset_pack.ItemIndex:=i;
+  { заполняем чек-боксы }
+  self.cb_play_music.Checked:=self.settings_manager.get_music_state_on;
+  self.cb_play_sounds.Checked:=self.settings_manager.get_sounds_state_on;
+  { заполняем трак-бары }
+  self.tb_sounds_volume.Position:=self.settings_manager.get_sounds_volume;
+  self.tb_music_volume.Position:=self.settings_manager.get_music_volume;
+end;
+
+procedure Tf_settings.tb_music_volumeChange(Sender: TObject);
+begin
+  self.settings_manager.set_music_volume(self.tb_music_volume.Position);
+end;
+
+procedure Tf_settings.tb_sounds_volumeChange(Sender: TObject);
+begin
+  self.settings_manager.set_sounds_volume(self.tb_sounds_volume.Position);
+end;
 
 end.
 
